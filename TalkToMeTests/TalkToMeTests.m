505e9b10e7dc58c9f7d36b162564203a0226f58c
Ian Perry <ianperry90@gmail.com>
2014-02-28 12:40:12 -0500

    Initial Commit

---
+++ TalkToMeTests/TalkToMeTests.m
@@ -0,0 +1,34 @@
+//
+//  TalkToMeTests.m
+//  TalkToMeTests
+//
+//  Created by Ian Perry on 2/28/14.
+//  Copyright (c) 2014 iperry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TalkToMeTests : XCTestCase
+
+@end
+
+@implementation TalkToMeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
